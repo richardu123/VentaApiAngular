5c7a4b9f01538e5493cb20e55c792f908673f24b
richard-dev <richard01pch@gmail.com>
2025-03-03 20:56:44 -0500

    Agregar archivos de proyecto.

---
+++ README.md
@@ -0,0 +1,1 @@
+# SistemaVentaAngularNet
---
+++ SistemaVentaAngular/ClientApp/src/app/interfaces/reporte.ts
@@ -0,0 +1,11 @@
+export interface Reporte {
+  numeroDocumento:string,
+  tipoPago: string,
+  fechaRegistro: string,
+  totalVenta: string,
+  categoria: string,
+  producto: string,
+  cantidad:number,
+  precio: string,
+  total: string
+}
